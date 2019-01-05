694c805217df866bc8260b47bcb6e61df1153637
TAKAHASHI,Toru <torutk@gmail.com>
2019-01-05 10:04:10 +0900

    fix to close command prompt after launch

--- package/windows/analogclock.bat
+++ package/windows/analogclock.bat
@@ -5,7 +5,7 @@ set JLINK_VM_OPTIONS=-Xms32m -Xmx64m -Xss256k ^
 -XX:MetaspaceSize=12m ^
 -XX:+UseSerialGC
 set DIR=%~dp0
-"%DIR%\java" %JLINK_VM_OPTIONS% ^
+"%DIR%\javaw" %JLINK_VM_OPTIONS% ^
 -m com.torutk.gadget.analogclock/com.torutk.gadget.analogclock.AnalogClockApp ^
 --scale=0.7 --x=0 --y=0 --fps=4 ^
 %*
