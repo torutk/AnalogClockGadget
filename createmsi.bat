e29e207f8b6fa34d8c7c6d26939cfb52b7d5f28d
TAKAHASHI,Toru <torutk@gmail.com>
2020-07-23 22:43:10 +0900

    Migrate to jpackage

--- createmsi.bat
+++ createmsi.bat
@@ -1,20 +1,20 @@
 @echo off
 
-javapackager -deploy -native msi ^
--v ^
--outdir dist -outfile AnalogClockGadget ^
--p dist -m AnalogClockGadget/com.torutk.gadget.analogclock.AnalogClockApp ^
--name "AnalogClock" ^
--BappVersion=0.4.2 ^
--title "Analog Clock Gadget" ^
--vendor Takahashi ^
--description "Analog Clock on desktop" ^
--BjvmOptions=-Xms32m ^
--BjvmOptions=-Xms128m ^
--BjvmOptions=-Xss256k ^
--BjvmOptions=-XX:TieredStopAtLevel=1 ^
--BjvmOptions=-XX:CICompilerCount=2 ^
--BjvmOptions=-XX:CompileThreshold=1500 ^
--BjvmOptions=-XX:InitialCodeCacheSize=160k ^
--BjvmOptions=-XX:ReservedCodeCacheSize=32m ^
--BjvmOptions=-XX:+UseSerialGC
+REM increment the version number each creation.
+set APP_VERSION=0.5.6
+
+%JAVA_HOME%\bin\jpackage ^
+--type msi ^
+--win-upgrade-uuid fe287b25-e03d-4744-90f4-96b05dc36bf0 ^
+--win-menu-group "High Bridge" ^
+--win-menu ^
+--win-shortcut ^
+--app-version %APP_VERSION% ^
+--description "Analog Clock on desktop" ^
+--name "AnalogClock" ^
+--dest build\installer ^
+--vendor "High Bridge" ^
+--module-path build\libs ^
+--module com.torutk.gadget.analogclock ^
+--java-options "-Xms32m -Xmx64m -Xss256k -XX:TieredStopAtLevel=1 -XX:CICompilerCount=2 -XX:CompileThreshold=1500 -XX:InitialCodeCacheSize=160k -XX:ReservedCodeCacheSize=32m -XX:MetaspaceSize=12m -XX:+UseSerialGC" ^
+--verbose
